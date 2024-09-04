99c5e0d1a6e0ae91f2c79dc92e7daab57b1ce553
K <me_oh_yeah@hotmail.com>
2024-09-04 01:58:12 -0400

    fix: tweak tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,24 +4,26 @@ from src.pytemplate.domain.models import Operand
 import pytest
 
 
-# initialization should always set class fields to 0
-def test_operand_initialization():
-    operand = Operand()
-    assert operand.first_operand == 0
-    assert operand.second_operand == 0
+# initial values
+def test_operand_assignment():
+    # initialization requires two values
+    operand = Operand(42, 15)
+    assert operand.first_operand == 42
+    assert operand.second_operand == 15
 
 
 # class fields will hold numeric values
-def test_operand_assignment():
-    operand = Operand()
-    operand.first_operand = 42
-    operand.second_operand = 15
-    assert operand.first_operand == 42
+def test_operand_reassignment():
+    operand = Operand(42, 15)
+    # reassignment
+    operand.first_operand = 5
+    assert operand.first_operand == 5
     assert operand.second_operand == 15
 
 
 # class fields will ERROR if assigned non-numeric values
 def test_operand_assignment_non_int():
+    operand = Operand(42, 15)
     with pytest.raises(TypeError):
         operand.first_operand = "not an integer"
         operand.second_operand = "also not an integer"
