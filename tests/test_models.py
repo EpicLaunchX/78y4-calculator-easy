476e6a25c58212aac79745db987abdcf9ff795bb
K <me_oh_yeah@hotmail.com>
2024-09-04 00:20:53 -0400

    feat: test models.py

---
+++ tests/test_models.py
@@ -0,0 +1,25 @@
+# tests/test_models.py
+import pytest
+
+
+# initialization should always set class fields to 0
+def test_operand_initialization():
+    operand = Operand()
+    assert operand.first_operand == 0
+    assert operand.second_operand == 0
+
+
+# class fields will hold numeric values
+def test_operand_assignment():
+    operand = Operand()
+    operand.first_operand = 42
+    operand.second_operand = 15
+    assert operand.first_operand == 42
+    assert operand.second_operand == 15
+
+
+# class fields will ERROR if assigned non-numeric values
+def test_operand_assignment_non_int():
+    with pytest.raises(TypeError):
+        operand.first_operand = "not an integer"
+        operand.second_operand = "also not an integer"
