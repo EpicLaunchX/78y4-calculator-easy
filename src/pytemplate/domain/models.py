e3ae579d7ff31028669c405c658a03b49b1aec72
K <me_oh_yeah@hotmail.com>
2024-09-04 02:31:25 -0400

    fix: error validation

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -2,8 +2,15 @@
 from dataclasses import dataclass
 
 
-# need a class for this
 @dataclass
 class Operand:
-    first_operand: int
-    second_operand: int
+    # Proper init function to validate input
+    def __init__(self, first_operand: int, second_operand: int):
+        # Raises error if value is not an integer
+        if not isinstance(first_operand, int):
+            raise TypeError("first_operand must be an integer")
+        if not isinstance(second_operand, int):
+            raise TypeError("second_operand must be an integer")
+        # Assigns values to class object
+        self.first_operand = first_operand
+        self.second_operand = second_operand
