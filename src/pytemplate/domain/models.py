f7e8f30e81430317449caec198d3a91922ddd421
K <me_oh_yeah@hotmail.com>
2024-09-08 22:52:20 -0400

    fix: formatting

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Operand():
+class Operand:
     # Init function just assigns value
     def __init__(self, first_operand: int, second_operand: int):
         self.first_operand = first_operand
@@ -12,5 +12,5 @@ class Operand():
     # SetAttr function performs type validation
     def __setattr__(self, attr, value):
         if not isinstance(value, int):
-            raise TypeError(f'{attr} must be type int')
+            raise TypeError(f"{attr} must be type int")
         super().__setattr__(attr, value)
